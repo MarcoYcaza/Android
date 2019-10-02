e5993cc33daafb9d61b9174a1ecc3bc14f5cf1af
MarcoYcaza <ycaza.marco@gmail.com>
2019-10-01 20:40:41 -0500

    Otro proyecto añadido

---
+++ ShareTextFeature/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView mTextView = findViewById(R.id.textView);
+        Button mBtnShare = findViewById(R.id.btnShare);
+        mBtnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = mTextView.getText().toString();
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, s);
+                startActivity(Intent.createChooser(sharingIntent, "Share text via"));
+            }
+        });
+
+    }
+
+}
